358906cee94d202e8eda8b1d4b80ce56904e2e37
sieun <21800405@handong.ac.kr>
2023-01-09 15:54:19 +0900

    create mail-master

---
+++ lib/home.dart
@@ -0,0 +1,219 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:cupertino_icons/cupertino_icons.dart';
+import 'package:mail/util/size.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import 'login.dart';
+import 'mail/mail_list/mail_screen.dart';
+import 'mail/sendMail_modal.dart';
+
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  final user = FirebaseAuth.instance.currentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF2F2F6),
+      body: SafeArea(
+        child: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (!snapshot.hasData) {
+                return const Login();
+              } else {
+                return ListView(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              IconButton(
+                                  onPressed: () {
+                                    FirebaseAuth.instance.signOut();
+                                  },
+                                  icon: Icon(
+                                    CupertinoIcons.square_arrow_left,
+                                    size: 20,
+                                    color: Colors.blueAccent,
+                                  )),
+                              Text(
+                                "편집",
+                                style: TextStyle(
+                                  color: Colors.blueAccent,
+                                  fontSize: 15,
+
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                "메일상자",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 30,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Container(
+                              padding: EdgeInsets.symmetric(vertical: 8),
+                              color: Colors.white,
+                              child: Column(
+                                children: [
+                                  _mainButton(CupertinoIcons.tray, "받은 편지함"),
+                                  Divider(indent: 50, color: Colors.grey),
+                                  _mainButton(CupertinoIcons.star, "VIP"),
+                                  Divider(indent: 50, color: Colors.grey),
+                                  _mainButton(CupertinoIcons.flag, "깃발 표시됨"),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                "${snapshot.data.displayName} 님",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Container(
+                              padding: EdgeInsets.symmetric(vertical: 8),
+                              color: Colors.white,
+                              child: Column(
+                                children: [
+                                  _mainButton(CupertinoIcons.doc, "임시 저장"),
+                                  Divider(indent: 50, color: Colors.grey),
+                                  _mainButton(
+                                      CupertinoIcons.paperplane, "보낸 편지함"),
+                                  Divider(indent: 50, color: Colors.grey),
+                                  _mainButton(CupertinoIcons.bin_xmark, "정크"),
+                                  Divider(indent: 50, color: Colors.grey),
+                                  _mainButton(CupertinoIcons.delete, "휴지통"),
+                                  Divider(indent: 50, color: Colors.grey),
+                                  _mainButton(
+                                      CupertinoIcons.archivebox, "모든 메일"),
+                                  Divider(indent: 50, color: Colors.grey),
+                                  _mainButton(
+                                    CupertinoIcons.folder,
+                                    "별표 편지함",
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          // Text(
+                          //   "${snapshot.data.displayName}님 환영합니다.",
+                          //   style: TextStyle(fontSize: 20),
+                          // )
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              }
+            }),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Color(0xFFF2F2F6),
+        elevation: 0.0,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0,0,12,12),
+          child: Row(
+            children: [
+              Spacer(),
+              IconButton(
+                  onPressed: () =>
+                      showModalBottomSheet(
+                    //enableDrag: false, isDismissible: false,
+                    isScrollControlled: true,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(20),
+                      ),
+                    ),
+                    context: context,
+                    builder: (context) => Container(
+                        height: getScreenHeight(context) * 0.9,
+                        child: MailModal()),
+                  ),
+                  icon: Icon(
+                    CupertinoIcons.pencil_outline,
+                    color: Colors.blueAccent,
+                    size: 30,
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _mainButton(IconData icon, String title) {
+    return GestureDetector(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(15.0, 2.0, 15.0, 0),
+        child: Stack(alignment: AlignmentDirectional.centerStart, children: [
+          Row(
+            children: [
+              Icon(
+                icon,
+                size: 20,
+                color: Colors.blueAccent,
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Text(
+                title,
+                style: TextStyle(fontSize: 15),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Icon(
+                CupertinoIcons.chevron_right,
+                size: 15,
+                color: Colors.grey,
+              ),
+            ],
+          ),
+        ]),
+      ),
+      onTap: () {
+        Get.to(MailScreen(title));
+      },
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MailApp(),
+    );
+  }
+}
